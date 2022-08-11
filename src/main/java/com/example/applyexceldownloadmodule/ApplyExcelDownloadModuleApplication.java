32ac4c9573f6155da8c2736f0bd3eebd4c802c8c
hyune <choi8608@gmail.com>
2022-08-11 14:09:26 +0900

    Initial commit

---
+++ src/main/java/com/example/applyexceldownloadmodule/ApplyExcelDownloadModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.applyexceldownloadmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyExcelDownloadModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplyExcelDownloadModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/applyexceldownloadmodule/ApplyExcelDownloadModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applyexceldownloadmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplyExcelDownloadModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
